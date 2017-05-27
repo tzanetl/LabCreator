c0737a5eac0bccc49cf13121a3eb512674ddbd71
Taneli <taneli.leppanen@hotmail.com>
2017-05-28 00:41:09 +0200

    Added print support to Lab

--- Lab.py
+++ Lab.py
@@ -15,3 +15,27 @@ class Lab(object):
             "start: {2}\n" \
             "tiles: {3}".format(self.w, self.h, self.start, self.tiles)
         return r
+
+    def __str__(self):
+
+        res = "h: {0} | w: {1} | start: {2}".format(self.h, self.w, self.start)
+
+        if self.tiles is not None:
+            row = "\n"
+
+            for row in self.tiles:
+
+                for tile in row:
+
+                    if tile.type is None:
+                        row += "N"
+
+                    else:
+                        row += tile.type
+
+                res += row
+
+        else:
+            res += "\nNo map"
+
+        return res
