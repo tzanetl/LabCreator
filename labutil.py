4268ea550569d55bc5a85ed05d89636461d49376
Taneli <taneli.leppanen@hotmail.com>
2017-05-28 00:34:35 +0200

    Implementation of labinit started

--- labutil.py
+++ labutil.py
@@ -3,10 +3,22 @@
 
 from random import randint
 from Lab import Lab
+from Tile import Tile
 
 
 #Initializes tiles with weights and types
 def labinit(h, w, start):
     lab = Lab()
+    tiles = []
 
+    for m in range(h):
+        row = []
+
+        for n in range(w):
+            tile = Tile(randint(1, 100))
+            row.append(tile)
+
+        tiles.append(row)
+
+    lab.tiles = tiles
     return lab
